3e04153fdec9b062398b5de338720eb62921ed3d
Austin Long <austinlong@MacBook-Pro-71.local>
2018-09-08 13:04:29 -0700

    Hw02a

--- hw0pr2a.py
+++ hw0pr2a.py
@@ -11,7 +11,7 @@ def rps():
        arguments: no arguments    (prompted text doesn't count as an argument)
        results: no results        (printing doesn't count as a result)
    """
-   user = input("Choose your weapon: ")
+   user = input("Choose your weapon: (rock, paper, or scissors) ")
    comp = random.choice(['rock','paper','scissors'])
    print()
 
@@ -19,11 +19,35 @@ def rps():
    print('The computer (I) chose', comp)
    print()
 
+   if user != ('rock' or 'paper' or 'scissors'):
+       print("Invalid weapon selection! Play again!")
+       rps()
+
    if user == 'rock' and comp == 'paper':
        print('I WIN! HAHAHAHAHA, you can not defeat the machine!')
        print('Better luck next time...')
    elif user == 'rock' and comp == 'rock':
        print('Wow, it looks like we tied! Let\'s go again')
        rps()
-   else:
-      print('It looks like you have bested me... :( It will not happen again')
+   elif user == 'rock' and comp == 'scissors':
+      print('Rock crushes scissors! It looks like you have bested me... :( It will not happen again.')
+
+
+   if user == 'paper' and comp == 'scissors':
+       print('I WIN! Scissors cuts paper! HAHAHA, you can not defeat the machine!')
+       print('Better luck next time...')
+   elif user == 'paper' and comp == 'paper':
+       print('Wow, it looks like we tied! Let\'s go again')
+       rps()
+   elif user == 'paper' and comp == 'rock':
+       print('Paper smothers Rock! It looks like you have bested me... :( It will not happen again.')
+
+
+   if user == 'scissors' and comp == 'rock':
+       print('I WIN! Rock crushes scissors! HAHAHAHAHA, you can not defeat the machine!')
+       print('Better luck next time...')
+   elif user == 'scissors' and comp == 'scissors':
+       print('Wow, it looks like we tied! Let\'s go again')
+       rps()
+   elif user == 'scissors' and comp == 'paper':
+       print('Scissors cuts paper! It looks like you have bested me... :( It will not happen again.')
