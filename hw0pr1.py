af4498bbc6a7cde6db4f970ec8d7f4b820c5b11f
Austin Long <austinlong@MacBook-Pro-71.local>
2018-09-09 21:41:40 -0700

    Added Dodd Quest in title

---
+++ hw0pr1.py
@@ -0,0 +1,24 @@
+# CS5 Gold/Black: Lab 0, Problem 1
+# Filename: hw0pr1.py
+# Name:
+# Problem description: The four fours challenge!
+
+from math import *
+
+print("Zero is", int(4+4-4-4))
+print("One is", int(4*4/4/4))
+print("Two is", int(((4*4)/sqrt(4))/4))
+print("Three is", int(((4*4)-4)/4))
+print("Four is", int((factorial(4)-4-4)/4))
+print("Five is", int((4*4+4)/4))
+print("Six is", int((4+4)/4 + 4))
+print("Seven is", int(factorial(4)/4 + (4/4)))
+print("Eight is", int(4*4/4+4))
+print("Nine is", int(4/4+4+4))
+print("Ten is", int((44-4)/4))
+print("Eleven is", int((factorial(4) + 4)/4 + 4))
+print("Twelve is", int((4*4)/sqrt(4)+4))
+print("Thirteen is", int(factorial(4) - (44/4)))
+print("Fourteen is", int((factorial(4)/4)+4+4))
+print("Fifteen is", int(4 * 4 -(4/4)))
+print("Sixteen is", int(4*4*4/4))
